bcc25a9a9e6eca38583d05c2c2a5e1a4b66442fc
sohilp393 <Sohilp393@gmail.com>
2020-05-08 19:27:33 +0530

    Added validations for contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,9 @@
 class Contact < ActiveRecord::Base
 
-
+	validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :email, presence: true, uniqueness: true
+  	
   def name
     [firstname,lastname].join(' ')
   end
