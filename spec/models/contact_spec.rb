8d36b7a28b7479a46dbe9b99688083ab2d618854
sohilp393 <Sohilp393@gmail.com>
2020-05-11 21:20:17 +0530

    Refactor the test cases

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 require 'faker'
 
 RSpec.describe Contact, type: :model do
+
   it 'is valid with a firstname,lastname and email' do
     @contact = build(:contact)
     expect(@contact).to be_valid
@@ -24,16 +25,19 @@ RSpec.describe Contact, type: :model do
   it 'is invalid without a email address' do
     @contact = build(:contact, email: nil)
     @contact.valid?
-    expect(@contact.errors[:email]).not_to include("can't be blank")
+    expect(@contact.errors[:email]).to include("can't be blank")
   end
 
   it 'is invalid with a duplicate email address' do
-    Contact.create(
+    create(
+      :contact,
       first_name: 'Joe',
       last_name: 'Button',
       email: 'tester@example.com'
     )
-    @contact = Contact.new(
+
+    @contact = build(
+      :contact,
       first_name: 'Jane',
       last_name: 'Button',
       email: 'tester@example.com'
@@ -54,18 +58,21 @@ RSpec.describe Contact, type: :model do
 
   describe 'filter last_name by letter' do
     before :each do
-      @smith = Contact.create(
+      @smith = create(
+        :contact,
         first_name: 'John',
         last_name: 'Smith',
         email: 'jsmith@example.com'
       )
 
-      @jones = Contact.create(
+      @jones = create(
+        :contact,
         first_name: 'Tim',
         last_name: 'Jones',
         email: 'tjones@example.com'
       )
-      @johnson = Contact.create(
+      @johnson = create(
+        :contact,
         first_name: 'John',
         last_name: 'Johnson',
         email: 'jjohnson@example.com'
