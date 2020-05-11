8aca83cb3de383f2fff51240a8b849543f1987f0
sohilp393 <Sohilp393@gmail.com>
2020-05-11 18:03:56 +0530

    Added phone factory data

---
+++ spec/factories/phones.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :phone do
+    association :contact
+    phone { Faker::PhoneNumber.phone_number }
+
+    factory :home_phone do
+      phone_type 'home'
+    end
+
+    factory :work_phone do
+      phone_type 'work'
+    end
+
+    factory :mobile_phone do
+      phone_type 'mobile'
+    end
+  end
+end
