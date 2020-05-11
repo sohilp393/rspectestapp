86276421572a013bb2192be61e2ea7cef8119961
sohilp393 <Sohilp393@gmail.com>
2020-05-11 17:58:49 +0530

    Factory data for contact specs

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -1,6 +1,13 @@
+# frozen_string_literal: true
+
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
   factory :contact do
+    first_name	{ Faker::Name.first_name }
+    last_name	{ Faker::Name.last_name }
+    # sequence is a feature of factorygirl
+    # sequence(:email) { |n| "johndoe#{n}@example.com"}
+    email	{ Faker::Internet.email }
   end
 end
