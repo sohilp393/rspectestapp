e2ecf7ec8e9c549b999eaa88448dfcf129baeca9
sohilp393 <Sohilp393@gmail.com>
2020-05-12 12:01:50 +0530

    ..

--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -5,13 +5,13 @@ require 'faker'
 
  describe Phone, type: :model do
     it 'does not allow duplicate phone number per contact' do
-      contact = create(:contact)
+      create(:contact)
       create(:home_phone,
 			contact: contact,
 			phone: '785-555-1234'
 		)
     mobile_phone = build(:mobile_phone, contact: contact, phone: '785-555-1234')
-		  mobile_phone.valid?
+		mobile_phone.valid?
     expect(mobile_phone.errors[:phone]).to include('has already been taken')
   end
   it 'allows two contacts to share a phone number' do
