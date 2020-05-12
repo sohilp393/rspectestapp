3bdc4169e3092157359d1abeb89e1467c2c5eae3
sohilp393 <Sohilp393@gmail.com>
2020-05-12 13:52:13 +0530

    Refactor the code using factories.

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -6,8 +6,31 @@ FactoryGirl.define do
   factory :contact do
     first_name	{ Faker::Name.first_name }
     last_name	{ Faker::Name.last_name }
-    # sequence is a feature of factorygirl
-    # sequence(:email) { |n| "johndoe#{n}@example.com"}
     email	{ Faker::Internet.email }
+
+  	factory :joe do
+  		first_name 'Joe'
+  		last_name 'Button'
+  		email 'tester@example.com'
+  	end
+
+    factory :jane do
+  		first_name 'Jane'
+  		last_name 'Button'
+  		email 'tester@example.com'
+  	end
+
+  	factory :jones do
+  		first_name 'John'
+  		last_name 'Jones'
+  		email 'jones@example.com'	
+  	end	
+  	
+  	factory :wick do
+  		first_name 'John'
+  		last_name 'Wick'
+  		email 'wick@example.com'	
+  	end
+
   end
 end
